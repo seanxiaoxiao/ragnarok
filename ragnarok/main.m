10ec8739f41896d8f8cc7d0665ee423693f3246e
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2013-01-19 09:30:04 -0800

    Initial Commit

---
+++ ragnarok/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ragnarok
+//
+//  Created by Xiao Xiao on 1/19/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
